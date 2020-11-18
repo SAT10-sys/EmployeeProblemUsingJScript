f196bb951539cc32f32c6231de7284644733b514
Satantra Tewari <satantra1508@gmail.com>
2020-11-18 15:53:11 +0530

    [satantra] refactor. added function to calculate employee hours

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -4,18 +4,21 @@ const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
 let employeeCheck=Math.floor(Math.random()*10)%3;
-let employeeHours=0;
+let employeeHours=GetEmployeeHours(employeeCheck);
+let employeeWage=employeeHours*WAGE_PER_HOUR;
+console.log("Daily Wage: "+employeeWage);
+function GetEmployeeHours(employeeCheck)
+{
 switch(employeeCheck)
 {
     case IS_PRESENT_FULL_TIME:
-        employeeHours=FULL_TIME_HOURS;
+        return FULL_TIME_HOURS;
         break;
     case IS_PRESENT_PART_TIME:
-        employeeHours=PART_TIME_HOURS;
+        return PART_TIME_HOURS;
         break;
     default:
-        empHours=0;
+        return 0;
         break;
 }
-let employeeWage=employeeHours*WAGE_PER_HOUR;
-console.log("Daily Employee Wage: "+employeeWage);
+}
