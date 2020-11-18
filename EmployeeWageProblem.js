1266cad5e7628e001b2dee07dd6299517945cf0b
Satantra Tewari <satantra1508@gmail.com>
2020-11-18 15:45:31 +0530

    [satantra] calculated daily employee wage

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -1,7 +1,21 @@
-//UC1 Checking Employee Present Or Absent
-const IS_PRESENT=1;
-let employeeCheck=Math.floor(Math.random()*10)%2;
-if(employeeCheck==IS_PRESENT)
-console.log("Employee is Present");
-else
-console.log("Employee is Absent");
+const IS_PRESENT_FULL_TIME=2;
+const IS_PRESENT_PART_TIME=1;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOURS=8;
+const WAGE_PER_HOUR=20;
+let employeeCheck=Math.floor(Math.random()*10)%3;
+let employeeHours=0;
+switch(employeeCheck)
+{
+    case IS_PRESENT_FULL_TIME:
+        employeeHours=FULL_TIME_HOURS;
+        break;
+    case IS_PRESENT_PART_TIME:
+        employeeHours=PART_TIME_HOURS;
+        break;
+    default:
+        empHours=0;
+        break;
+}
+let employeeWage=employeeHours*WAGE_PER_HOUR;
+console.log("Daily Employee Wage: "+employeeWage);
