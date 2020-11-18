510cb9f004570925c0fb16b0a2c1cf93141cf6cc
Satantra Tewari <satantra1508@gmail.com>
2020-11-18 15:59:37 +0530

    [satantra] calculated employee wages for a month

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -3,10 +3,17 @@ const IS_PRESENT_PART_TIME=1;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
-let employeeCheck=Math.floor(Math.random()*10)%3;
-let employeeHours=GetEmployeeHours(employeeCheck);
+const WORKING_DAYS_PER_MONTH=20;
+
+
+let employeeHours=0;
+for(i=0;i<WORKING_DAYS_PER_MONTH;++i)
+{
+    let employeeCheck=Math.floor(Math.random()*10)%3;
+    employeeHours=employeeHours+GetEmployeeHours(employeeCheck);
+}
 let employeeWage=employeeHours*WAGE_PER_HOUR;
-console.log("Daily Wage: "+employeeWage);
+console.log("Total Working Hours: "+employeeHours+"\nTotal Monthly Wages: "+employeeWage);
 function GetEmployeeHours(employeeCheck)
 {
 switch(employeeCheck)
